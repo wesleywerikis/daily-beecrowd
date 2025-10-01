a68580224b18320beb68331b78819163bf68a982
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-01 11:35:18 -0300

    feat: Add solution for Beecrowd 1000 - Hello Word

---
+++ src/main/java/br/com/beecrowd/iniciante/BEE_1000/Main.java
@@ -0,0 +1,32 @@
+package br.com.beecrowd.iniciante.BEE_1000;
+
+import java.io.IOException;
+
+/**
+ * Beecrowd | 1000
+ * "Hello World!"
+ *
+ * 📌 Enunciado:
+ * O seu primeiro programa em qualquer linguagem normalmente é o "Hello World!".
+ * Neste problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+ *
+ * Entrada:
+ * - Não há nenhuma entrada.
+ *
+ * Saída:
+ * - A saída deve ser a mensagem "Hello World!" seguida de uma quebra de linha.
+ *
+ * 💡 Raciocínio:
+ * Basta imprimir exatamente o texto pedido com System.out.println.
+ *
+ * Exemplo:
+ * Saída esperada:
+ * Hello World!
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+
+}
